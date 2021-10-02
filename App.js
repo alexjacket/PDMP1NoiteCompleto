b09cae888cc73a19199642c7f732ee544e14b4b7
Alexandre Giachetta <alexjacket@outlook.com>
2021-10-02 18:24:10 -0300

    Atualização nome dos integrantes

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+  View
+} from 'react-native';
+import ExibeItens from './components/ExibeItens';
+import EntradaDeDados from './components/EntradaDeDados';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  //const [itens, setItens] = useState([]) //ele vai servir para trazer a descricao e os fabricantes usando uma JSX
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      itens: [],
+      contador: 0
+    }
+  }
+
+  render() {
+    
+
+      const inserirItens = (fabricante, descricao) =>{
+
+        let itemInserido = {
+          fabricante: fabricante,
+          descricao: descricao,
+          key: this.state.contador.toString()
+        }
+
+        this.setState({
+          itens: [...this.state.itens, itemInserido],
+          contador: this.state.contador +1
+        })
+        console.log(this.state.itens)
+        
+
+      }
+      return (
+      <View>
+        <EntradaDeDados funcao={inserirItens} />
+        <ExibeItens itens={this.state.itens} />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
